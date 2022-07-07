ccb8a99f2eca5dcbddafba057b069c453097112f
mtardy <mtardy@quarkslab.com>
2022-07-07 20:38:30 +0200

    Add a template generator command to create bad pod

---
+++ commands/gen.go
@@ -0,0 +1,77 @@
+package commands
+
+import (
+	"os"
+
+	"github.com/quarkslab/kdigger/pkg/kgen"
+	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/printers"
+)
+
+var opts kgen.GenerateOpts
+
+var genAll bool
+
+var genCmd = &cobra.Command{
+	Use:     "gen [name] [flags]",
+	Aliases: []string{"generate"},
+	Short:   "Generate template for pod with security features disabled",
+	Long: `This command generates templates for pod with security features disabled.
+You can customize the pods with some of the string flags and activate
+boolean flags to disabled security features. Examples:
+
+  # Generate a very simple template in json
+  kdigger gen -o json
+
+  # Create a very simple pod
+  kdigger gen | kubectl apply -f -
+
+  # Create a pod named mypod with most security features disabled
+  kdigger gen -all mypod | kubectl apply -f -
+  
+  # Create a custom privileged pod
+  kdigger gen --privileged --image bash --command watch --command date | kubectl apply -f -`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// all puts all the boolean flags to true
+		if genAll {
+			opts.HostNetwork = true
+			opts.Privileged = true
+			opts.HostPath = true
+			opts.HostPid = true
+			opts.Tolerations = true
+		}
+		if len(args) > 0 {
+			opts.Name = args[0]
+		}
+
+		pod := kgen.Generate(opts)
+
+		var p printers.ResourcePrinter
+		if output == "json" {
+			p = &printers.JSONPrinter{}
+		} else {
+			p = &printers.YAMLPrinter{}
+		}
+		err := p.PrintObj(pod, os.Stdout)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(genCmd)
+
+	genCmd.Flags().StringVar(&opts.Image, "image", "busybox", "Container image used")
+	genCmd.Flags().StringArrayVar(&opts.Command, "command", []string{"sleep", "infinitely"}, "Container command used")
+
+	genCmd.Flags().BoolVar(&opts.Privileged, "privileged", false, "Add the security flag to the security context of the pod")
+	genCmd.Flags().BoolVar(&opts.HostPath, "hostpath", false, "Add a hostPath volume to the container")
+	genCmd.Flags().BoolVar(&opts.Tolerations, "tolerations", false, "Add tolerations to be schedulable on most nodes")
+	genCmd.Flags().BoolVar(&opts.HostPid, "hostpid", false, "Add the hostPid flag on the whole pod")
+	genCmd.Flags().BoolVar(&opts.HostNetwork, "hostnetwork", false, "Add the hostNetwork flag on the whole pod")
+
+	genCmd.Flags().BoolVar(&genAll, "all", false, "Enable everything")
+}
---
+++ pkg/kgen/kgen.go
@@ -0,0 +1,97 @@
+package kgen
+
+import (
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/apimachinery/pkg/util/rand"
+)
+
+type GenerateOpts struct {
+	Name        string
+	Image       string
+	Command     []string
+	Privileged  bool
+	HostPath    bool
+	HostPid     bool
+	HostNetwork bool
+	Tolerations bool
+}
+
+func Generate(opts GenerateOpts) *v1.Pod {
+	pod := &v1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Command: []string{"sleep", "infinitely"},
+					Name:    "digger",
+					Image:   "busybox",
+				},
+			},
+		},
+	}
+
+	if opts.Name == "" {
+		pod.ObjectMeta.Name = "digger-" + rand.String(5)
+	} else {
+		pod.ObjectMeta.Name = opts.Name
+		pod.Spec.Containers[0].Name = opts.Name
+	}
+
+	if opts.Image != "" {
+		pod.Spec.Containers[0].Image = opts.Image
+	}
+
+	if len(opts.Command) != 0 {
+		pod.Spec.Containers[0].Command = opts.Command
+	}
+
+	pod.Spec.HostPID = opts.HostPid
+
+	pod.Spec.HostNetwork = opts.HostNetwork
+
+	if opts.Privileged {
+		pod.Spec.Containers[0].SecurityContext = &v1.SecurityContext{
+			Privileged: &opts.Privileged,
+		}
+	}
+
+	if opts.HostPath {
+		pod.Spec.Volumes = append(pod.Spec.Volumes, v1.Volume{
+			Name: "host",
+			VolumeSource: v1.VolumeSource{
+				HostPath: &v1.HostPathVolumeSource{
+					Path: "/",
+				},
+			},
+		})
+		pod.Spec.Containers[0].VolumeMounts = append(pod.Spec.Containers[0].VolumeMounts, v1.VolumeMount{
+			Name:      "host",
+			MountPath: "/hostfs",
+		})
+	}
+
+	if opts.Tolerations {
+		pod.Spec.Tolerations = append(pod.Spec.Tolerations, []v1.Toleration{
+			{
+				Key:      "NoExecute",
+				Operator: "Exists",
+			},
+			{
+				Key:      "NoSchedule",
+				Operator: "Exists",
+			},
+			{
+				Key:      "CriticalAddonsOnly",
+				Operator: "Exists",
+			},
+		}...)
+	}
+
+	return pod
+}
